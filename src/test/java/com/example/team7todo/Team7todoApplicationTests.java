e0ed68b6f3d18c43a6e6d0afb92ab39458e87b4a
Woozy <anfrosus@naver.com>
2022-10-14 16:31:27 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/team7todo/Team7todoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team7todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7todoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
